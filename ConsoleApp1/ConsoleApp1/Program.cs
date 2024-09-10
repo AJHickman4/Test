751d19dd1c257f2b0482d99ccd04e2dad21a92bd
Alec Hickman <130108310+AJHickman4@users.noreply.github.com>
2024-09-10 18:14:35 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BeepBoopTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("beep boop test");
+            Console.ReadLine(); // Wait for user input before closing
+        }
+    }
+}
