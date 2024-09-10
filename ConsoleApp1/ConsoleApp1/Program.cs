7b42065f9bc5f3c6cf1eefa7afdb84ce95c1b594
AJHickman4 <ajhickman@student.fullsail.edu>
2024-09-10 18:17:28 -0500

    Test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace BeepBoopTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("beep boop test");
+            Console.WriteLine("No man it goes beep boop boop bop boop boop bop");
             Console.ReadLine(); // Wait for user input before closing
         }
     }
